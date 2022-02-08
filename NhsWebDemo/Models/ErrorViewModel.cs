76a10be2a549d09ad59650e09785011828331033
Andy Donegan <andy_donegan@hotmail.co.uk>
2022-02-08 20:19:33 +0000

    Add project files.

---
+++ NhsWebDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NhsWebDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
